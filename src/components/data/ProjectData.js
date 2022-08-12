766286d99cf4174b3c01b2ddfed1da7e5f21c55e
Tobinchilongo <tobinchilongojnr@gmail.com>
2022-08-12 19:35:31 +0200

    Add Links

--- src/components/data/ProjectData.js
+++ src/components/data/ProjectData.js
@@ -5,7 +5,7 @@ import todolist from '../../images/projects/todolist.png';
 
 export const projects = [
   {
-    id: 1, status: 'Development', title: 'Math Magicians', img: pww, projectLink: 'https://github.com/HAMMAS-SALEEM/Pakistan-Watch-Week-2022', liveLink: 'https://hammas-saleem.github.io/Pakistan-Watch-Week-2022/',
+    id: 1, status: 'Development', title: 'Math Magicians', img: pww, projectLink: 'https://github.com/Tobinchilongo/Math-Magicians', liveLink: 'https://mathmagician1.herokuapp.com/',
   },
   {
     id: 1, status: 'Development', title: 'MyBudget', img: budgetApp, projectLink: 'https://github.com/Tobinchilongo/Budget-App', liveLink: 'https://budget-app-zm.herokuapp.com/',
